da19fbc7c98ebbe0b58371a166c732d2f2e5dc64
itznik <75500096+itznik@users.noreply.github.com>
2025-10-19 20:09:40 +0530

    Update sanity.client.ts

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,5 +1,6 @@
 import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types' // <-- Import this type
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 export const dataset = 'production';
@@ -9,10 +10,12 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, 
+  useCdn: true,
 });
 
 const builder = imageUrlBuilder(client);
-export const urlFor = (source: any) => {
+
+// Update the type from 'any' to 'SanityImageSource'
+export const urlFor = (source: SanityImageSource) => {
   return builder.image(source);
 };
