2a541ca1c206dc1b039501ca22665ebe1ff84e2c
itznik <75500096+itznik@users.noreply.github.com>
2025-10-19 11:56:39 +0530

    Create sanity.client.ts

---
+++ src/lib/sanity.client.ts
@@ -0,0 +1,20 @@
+import { createClient } from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url'
+
+// IMPORTANT: Get these from your sanity.io project dashboard
+export const projectId = 'YOUR_PROJECT_ID';
+export const dataset = 'production';
+const apiVersion = '2023-05-03';
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true, // `false` if you want to ensure fresh data
+});
+
+// Helper function for generating image URLs with optimal settings
+const builder = imageUrlBuilder(client);
+export const urlFor = (source: any) => {
+  return builder.image(source);
+};
